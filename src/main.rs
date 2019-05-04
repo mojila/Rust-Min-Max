1ace310de723de988e2f41bbb4d343c78581bae0
mojila <me.mojila@gitlab.com>
2019-05-04 19:43:13 +0700

    add transform data from file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let filename = &args[1];
 
-    println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(&filename)
+        .expect("Something went wrong reading the file");
+
+    let trimmed = &contents.trim();
+
+    // check trimmed
+    //println!("Trimmed:\n{}", trimmed);
+
+    let lines = trimmed.lines().clone();
+
+    let mut splitted: Vec<Vec<i32>> = Vec::<Vec<i32>>::new();
+
+    // if all value is number and filled
+    for line in lines {
+        let numbers: Vec<i32> = line
+            .split(",")
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        splitted.push(numbers);
+    }
+
+    for split in &splitted {
+        println!("{:?}", split);
+    }
 }
